ab16c9072385b1290e5480f4b5f373d7864a7361
Stuart Leeks <stuart@leeks.net>
2016-04-25 12:44:09 +0100

    Strip path from class name for aspnet:class

--- Class/index.js
+++ Class/index.js
@@ -8,13 +8,17 @@ var NamedGenerator = module.exports = function NamedGenerator() {
 
 util.inherits(NamedGenerator, ScriptBase);
 
+NamedGenerator.prototype.getLastPathSegment = function(path) {
+  var index = Math.max(path.lastIndexOf("\\"), path.lastIndexOf("/"));
+  return path.substring(index+1);
+}
 NamedGenerator.prototype.createNamedItem = function() {
   var extension = '.cs';
   this.generateTemplateFile(
     'class.cs',
     extension, {
       namespace: this.namespace(),
-      classname: this.classNameWithoutExtension(extension)
+      classname: this.getLastPathSegment(this.classNameWithoutExtension(extension))
     }
   );
 };
